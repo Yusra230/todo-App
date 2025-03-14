c5a2ece4d29c045dc98feeb0daf23460cd6bf5d3
yusra230 <yusra4501434@cloud.neduet.edu.pk>
2025-03-14 16:26:38 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
 function App() {
 
   return (
       <>
-      <div>Hello World</div>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 ">Hello, Tailwind + React!</h1>
+    </div>
       </>
   )
 }
